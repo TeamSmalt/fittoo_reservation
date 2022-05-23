7f1b856c8d08f95bc9bb6e5786a3c61bc7d14ea6
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2022-05-23 17:05:41 +0900

    🚚 routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Calendar from 'components/Calendar';
 import React from 'react';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
 }
 
 export default App;
