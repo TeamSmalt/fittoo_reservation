c4996ee70bc1547406d7f9bab31ae82b1a3bc5c1
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2022-05-23 18:53:28 +0900

    🚚 routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import ReservationPage from 'pages/reservation/ReservationPage';
+
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="fittoo">
+      <Routes>
+        <Route path="reservation/:coachId" element={<ReservationPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
