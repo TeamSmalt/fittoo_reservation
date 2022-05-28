7916e4131ad2ad15e8e78952e503a68b6a39548c
Seongseokwon <pam9411@naver.com>
2022-05-28 16:15:52 +0900

    로그인 버튼 기본작업

--- src/containers/auth/AuthContainer.tsx
+++ src/containers/auth/AuthContainer.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
+import Logo from 'assets/svg/Logo';
+import GoogleLogo from 'assets/svg/GoogleLogo';
+import AppleLogoWhite from 'assets/svg/AppleLogoWhite';
+import KakaoLogo from 'assets/svg/KakaoLogo';
+import { Link } from 'react-router-dom';
+
 const AuthContainer = () => {
-  return <div className="auth">로그인 선택 페이지</div>;
+  return (
+    <div className="auth">
+      <section className="logo-section">
+        <Logo />
+      </section>
+
+      <section className="login-btn-group">
+        <button className="kakao-login">
+          <KakaoLogo />
+          카카오 로그인
+        </button>
+
+        <button className="apple-login">
+          <AppleLogoWhite />
+          애플 로그인
+        </button>
+
+        <button className="google-login">
+          <GoogleLogo />
+          구글 로그인
+        </button>
+
+        <button className="phone-login">휴대폰 번호 로그인</button>
+      </section>
+
+      <section className="footer">
+        <div className="signup-box">
+          아직 핏투 회원이 아니신가요?
+          <b>
+            <Link to="/signup"> 회원가입</Link>
+          </b>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+    </div>
+  );
 };
 
 export default AuthContainer;
