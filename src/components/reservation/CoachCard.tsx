987900a3807a8ab2b98d6330639064e062712e2b
SungSeokMin <jkl154527@gmail.com>
2022-05-30 22:52:53 +0900

    📝 rate 변경

--- src/components/reservation/CoachCard.tsx
+++ src/components/reservation/CoachCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Message from 'assets/svg/Message';
 import Phone from 'assets/svg/Phone';
 import DefaultImage from 'assets/svg/DefaultImage';
+import RatingStar from 'assets/svg/RatingStar';
 
 type CoachCardProps = {};
 
@@ -18,8 +19,8 @@ const CoachCard = ({}: CoachCardProps) => {
           근성장 보장, 체계적인 식단관리로 확실하게 변화시켜드립니다.
         </span>
         <div className="coach-card__rate">
-          <div className="coach-card__rate-start"></div>
-          <span>4.82(122)</span>
+          <RatingStar />
+          4.82(122)
         </div>
         <div className="coach-card__send">
           <a href="tel:01055879732">
