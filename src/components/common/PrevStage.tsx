a8f4fbbed994cab9da05d10eb19431ba9530983d
SungSeokMin <jkl154527@gmail.com>
2022-06-01 19:43:54 +0900

    📝 버튼 기능 추가

--- src/components/common/PrevStage.tsx
+++ src/components/common/PrevStage.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import LeftArrow from 'assets/svg/LeftArrow';
+import { useNavigate } from 'react-router-dom';
 
 type PrevStageProps = {
   title: string;
-  onClick: () => void;
 };
 
-const PrevStage = ({ title, onClick }: PrevStageProps) => {
+const PrevStage = ({ title }: PrevStageProps) => {
+  const navigate = useNavigate();
+
+  const onPrev = () => navigate(-1);
+
   return (
     <div className="prev-stage">
-      <LeftArrow onClick={onClick} />
+      <LeftArrow onClick={onPrev} />
       <span>{title}</span>
     </div>
   );
--- src/containers/reservation/ReservationContainer.tsx
+++ src/containers/reservation/ReservationContainer.tsx
@@ -5,19 +5,12 @@ import PrevStage from 'components/common/PrevStage';
 import CoachCard from 'components/reservation/CoachCard';
 import SelectTime from 'components/reservation/SelectTime';
 
-import { useNavigate } from 'react-router-dom';
-
 type ReservationContainerProps = {};
 
 const ReservationContainer = ({}: ReservationContainerProps) => {
-  const navigate = useNavigate();
-
-  //! 이전 페이지로 이동
-  const handleMovePrevState = () => navigate(-1);
-
   return (
     <div className="reservation">
-      <PrevStage title="예약하기" onClick={handleMovePrevState} />
+      <PrevStage title="예약하기" />
 
       <CoachCard />
 
