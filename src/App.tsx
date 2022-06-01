a7db4661dd2718e1dd54f5d1941c76066ebb2a22
SungSeokMin <jkl154527@gmail.com>
2022-06-01 19:43:17 +0900

    🚚 management routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import SignupPage from 'pages/SignupPage';
 
+import { Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/login/LoginPage';
+import SignupPage from 'pages/signup/SignupPage';
+import ManagementPage from 'pages/management/ManagementPage';
 import ReservationPage from 'pages/reservation/ReservationPage';
+import ScheduleContainer from 'containers/management/ScheduleContainer';
+import MemberContainer from 'containers/management/MemberContainer';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="login" element={<LoginPage />}></Route>
         <Route path="signup" element={<SignupPage />}></Route>
+        <Route path="management" element={<ManagementPage />}>
+          <Route path="schedules" element={<ScheduleContainer />} />
+          <Route path="members" element={<MemberContainer />} />
+        </Route>
         <Route path="coaches/:coachId" element={<ReservationPage />} />
       </Routes>
     </div>
